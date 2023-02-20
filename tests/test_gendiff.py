3b2dbd4e946c502413f21dcb9a563d371faa7164
Artem Terenin <artemterenin@gmail.com>
2023-02-20 23:43:50 +0300

    add plain stylish test

---
+++ tests/test_gendiff.py
@@ -0,0 +1,9 @@
+from gendiff.generate_diff import generate_diff
+from tests.fixtures.results import flat_json_result
+
+def test_generate_diff():
+
+    first_file = './tests/fixtures/file1.json'
+    second_file = './tests/fixtures/file2.json'
+    difference = generate_diff(first_file, second_file)
+    assert difference == flat_json_result
