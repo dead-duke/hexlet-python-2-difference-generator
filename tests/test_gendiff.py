2eae346d1e33ad784e60495eca92cf4e54335a91
Artem Terenin <artemterenin@gmail.com>
2023-02-20 23:48:12 +0300

    refactoring

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,8 +1,8 @@
 from gendiff.generate_diff import generate_diff
 from tests.fixtures.results import flat_json_result
 
-def test_generate_diff():
 
+def test_generate_diff():
     first_file = './tests/fixtures/file1.json'
     second_file = './tests/fixtures/file2.json'
     difference = generate_diff(first_file, second_file)
