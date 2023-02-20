c7d84280f3b4748bed47d9abef63dc4deaf94bfe
Artem Terenin <artemterenin@gmail.com>
2023-02-21 01:27:11 +0300

    add yml test

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,9 +1,11 @@
 from gendiff.generate_diff import generate_diff
-from tests.fixtures.expected_results import flat_json_result
+from tests.fixtures.expected_results import flat_stylish_result
 
 
 def test_generate_diff():
     first_file = './tests/fixtures/file1.json'
     second_file = './tests/fixtures/file2.json'
-    difference = generate_diff(first_file, second_file)
-    assert difference == flat_json_result
+    flat_json_difference = generate_diff(first_file, second_file)
+    assert flat_json_difference == flat_stylish_result
+    flat_yml_difference = generate_diff(first_file, second_file)
+    assert flat_yml_difference == flat_stylish_result
