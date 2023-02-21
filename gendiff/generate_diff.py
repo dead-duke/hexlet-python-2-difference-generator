ce4e64bc27715e6bb81add5f32c3bcdfd195de60
Artem Terenin <artemterenin@gmail.com>
2023-02-22 01:38:13 +0300

    refactoring

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -4,7 +4,6 @@ from gendiff.formatters.get_formatted_data import get_formatted_data
 
 
 def generate_diff(first_file, second_file, format_name='stylish'):
-    print(first_file)
     first_file_data = get_data(first_file)
     second_file_data = get_data(second_file)
     compared_data = compare_data(first_file_data, second_file_data)
