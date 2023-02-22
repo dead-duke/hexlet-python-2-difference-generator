29b3f2c766cb7633dafc4c5599e3f7533688adad
Artem Terenin <artemterenin@gmail.com>
2023-02-22 09:25:01 +0300

    refactoring

--- gendiff/compare_data.py
+++ gendiff/compare_data.py
@@ -2,13 +2,14 @@ def is_nested(data):
     return isinstance(data, (dict, tuple, list, set))
 
 
-def compare_data(first_data, second_data):
+def get_compared_data(first_data, second_data):
     compared_data = {}
     all_keys = list(first_data) + list(second_data)
     for key in sorted(all_keys):
         if is_nested(first_data.get(key)) and is_nested(second_data.get(key)):
+            children = get_compared_data(first_data[key], second_data[key])
             compared_data[key] = {
-                'children': compare_data(first_data[key], second_data[key]),
+                'children': children,
                 'type': 'nested'}
         elif key not in second_data:
             compared_data[key] = {
--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,11 +1,11 @@
 from gendiff.parser import get_data
-from gendiff.compare_data import compare_data
+from gendiff.compare_data import get_compared_data
 from gendiff.formatters.get_formatted_data import get_formatted_data
 
 
 def generate_diff(first_file, second_file, format_name='stylish'):
     first_file_data = get_data(first_file)
     second_file_data = get_data(second_file)
-    compared_data = compare_data(first_file_data, second_file_data)
+    compared_data = get_compared_data(first_file_data, second_file_data)
     formatted_data = get_formatted_data(compared_data, format_name)
     return formatted_data
